78f02f9f370e02e2dfbbd32d5887679878773148
xStFtx <noskillz.exe@gmail.com>
2023-12-22 23:31:25 +0200

    2.0

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # Load environment variables
 load_dotenv()
 
-# Function to get API key with validation
 def get_api_key(env_variable_name: str) -> str:
     api_key = os.getenv(env_variable_name)
     if not api_key:
         raise ValueError(f"The environment variable '{env_variable_name}' is not set.")
     return api_key
 
-# Function to generate content
 def generate_content(api_key: str, prompt: str) -> None:
     try:
         genai.configure(api_key=api_key)
@@ -26,23 +24,30 @@ def generate_content(api_key: str, prompt: str) -> None:
     except Exception as e:
         logging.error(f"Error generating content: {e}")
 
-# Function to start and handle chat
-def handle_chat(api_key: str) -> None:
+def handle_chat(api_key: str, prompt: str) -> None:
     try:
         chat = genai.start_chat()
-        response = chat.send_message('Hi, I have some questions for you.')
-        logging.info(response.text)
-        response = chat.send_message('What is the weather today?')
+        response = chat.send_message(prompt)
         logging.info(response.text)
     except Exception as e:
         logging.error(f"Error in chat: {e}")
 
-# Main function to run the script
 def main() -> None:
     try:
         api_key = get_api_key('API_KEY')
-        generate_content(api_key, 'Tell me a story about a magic backpack.')
-        handle_chat(api_key)
+
+        while True:
+            choice = input("Choose mode - Content (C) or Chat (Ch): ").strip().lower()
+            if choice in ['c', 'ch']:
+                break
+            print("Invalid choice. Please enter 'C' for Content or 'Ch' for Chat.")
+
+        user_prompt = input("Please enter your prompt: ").strip()
+
+        if choice == 'c':
+            generate_content(api_key, user_prompt)
+        else:
+            handle_chat(api_key, user_prompt)
     except Exception as e:
         logging.error(f"Error in main function: {e}")
 
